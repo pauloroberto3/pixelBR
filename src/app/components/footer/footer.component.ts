6a5e27ea2bef3010124125912ec443b8699da805
pauloroberto3 <paulormds412@gmail.com>
2025-09-23 16:47:04 -0300

    Adicionando header

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  imports: [RouterModule]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
