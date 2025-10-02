29722ef6c6bea77e2fcaa6f8f821276500932c7f
pauloroberto3 <paulormds412@gmail.com>
2025-10-02 14:08:34 -0300

    Adicionando links na nav do AuthHeader

--- src/app/components/AuthHeader/AuthHeader.component.ts
+++ src/app/components/AuthHeader/AuthHeader.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth-header',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './authheader.component.html',
-  styleUrls: ['./authheader.component.css']
+  imports: [CommonModule, RouterModule],
+  templateUrl: './AuthHeader.component.html',
+  styleUrls: ['./AuthHeader.component.css']
 })
 export class AuthHeaderComponent {
   // Este componente é puramente visual, a lógica fica vazia.
