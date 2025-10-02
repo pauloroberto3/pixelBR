953d65d9a8027bbe4522544a5abc540e4377b4d7
pauloroberto3 <paulormds412@gmail.com>
2025-10-02 13:51:32 -0300

    Adicionando página de sobre nós

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CarrinhoComponent } from './pages/Carrinho/Carrinho.component';
 import { DetalheJogoComponent } from './pages/DetalheJogo/DetalheJogo.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -14,4 +15,5 @@ export const routes: Routes = [
   { path: 'catalogo', component: CatalogoComponent },
   { path: 'carrinho', component: CarrinhoComponent },
   { path: 'jogo/:id', component: DetalheJogoComponent },
+  {path: "sobre", component: SobreComponent },
 ];
